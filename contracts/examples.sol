1ebc4ba0f0b4fc30ecb0f2df886ba44bb0962544
"Kevin Chen" <"kvnchen11@gmail.com">
2022-02-20 16:59:18 +1100

    Created repo

---
+++ contracts/examples.sol
@@ -0,0 +1,14 @@
+
+
+pragma solidity ^0.6.0;
+
+contract SimpleStorage{
+
+    uint256 favouriteNumber = 5;
+    bool favouriteBool = true;
+    string favouriteString = "String";
+    int256 favouriteInt = -5;
+    address favouiteAddress = 0x5179902Dc5711F0bB6a98D9f527e93Ba596932fe;
+    bytes32 favouriteBytes = "cat";
+
+}
